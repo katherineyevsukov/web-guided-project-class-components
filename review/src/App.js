0786e0f8186be373e88aaa025fcd05ba01936ad8
Kat <kyevsukov@gmail.com>
2021-09-27 12:32:50 -0400

    review person component and click handler built

--- review/src/App.js
+++ review/src/App.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+const Person = ({ state, handleClick }) => {
+  return (<div>
+    <h1>
+      Hello {state.name}. You are {state.age} years old.
+    </h1>
+    <button onClick={handleClick}>
+      Change name
+    </button>
+  </div>)
+}
 
 const App = () => {
+  const [state, setState] = useState({
+    name: "Warren",
+    age: 33,
+  });
+
+  const handleClick = () => {
+    setState({
+      ...state,
+      name: (state.name === "Warren" ? "Allison" : "Warren"),
+      age: 12
+    })
+  }
+
   return (
     <div>
-      <h1>Hello Warren</h1>
+      <Person state={state} handleClick={handleClick}/>
     </div>
   );
 };
